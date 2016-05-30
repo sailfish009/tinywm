d97601b25b4752c6febfef4363ed584a90afcde1
Uriel <uriel@berlinblue.org>
2016-05-29 20:55:10 -0400

    modify tinywm to my taste

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 PREFIX?=/usr/X11R6
-CFLAGS?=-Os -pedantic -Wall
+CFLAGS?=-Os -pedantic -Wall -std=c++11
 
 all:
-	$(CC) $(CFLAGS) -I$(PREFIX)/include tinywm.c -L$(PREFIX)/lib -lX11 -o tinywm
+	g++ $(CFLAGS) -I$(PREFIX)/include tinywm.c -L$(PREFIX)/lib -lX11 -o tinywm
 
 clean:
 	rm -f tinywm
--- tinywm.c
+++ tinywm.c
@@ -3,49 +3,70 @@
  * This software is in the public domain
  * and is provided AS IS, with NO WARRANTY. */
 
+#include <stdlib.h>
 #include <X11/Xlib.h>
-
+#include <X11/keysym.h>
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int main(void)
 {
-    Display * dpy;
-    XWindowAttributes attr;
-    XButtonEvent start;
-    XEvent ev;
+  Display * dpy;
+  XWindowAttributes attr;
+  XButtonEvent start{0};
+  XEvent ev;
+   
+  if(!(dpy = XOpenDisplay(0x0))) return 1;
 
-    if(!(dpy = XOpenDisplay(0x0))) return 1;
+  KeyCode RK = XKeysymToKeycode(dpy, XStringToKeysym("r"));
+  KeyCode EK = XKeysymToKeycode(dpy, XK_Escape);
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask,
-            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-    XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
-            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
-            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+  XGrabKey(dpy, RK, Mod1Mask,
+    DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
 
-    start.subwindow = None;
-    for(;;)
+  XGrabKey(dpy, EK, Mod1Mask,
+    DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+  XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
+    ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+
+  XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
+    ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+
+  start.subwindow = None;
+  for(;;)
+  {
+    XNextEvent(dpy, &ev);
+    switch(ev.type)
     {
-        XNextEvent(dpy, &ev);
-        if(ev.type == KeyPress && ev.xkey.subwindow != None)
-            XRaiseWindow(dpy, ev.xkey.subwindow);
-        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
-        {
-            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-            start = ev.xbutton;
-        }
-        else if(ev.type == MotionNotify && start.subwindow != None)
-        {
-            int xdiff = ev.xbutton.x_root - start.x_root;
-            int ydiff = ev.xbutton.y_root - start.y_root;
-            XMoveResizeWindow(dpy, start.subwindow,
-                attr.x + (start.button==1 ? xdiff : 0),
-                attr.y + (start.button==1 ? ydiff : 0),
-                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
-        }
-        else if(ev.type == ButtonRelease)
-            start.subwindow = None;
+    case KeyPress:
+      if(ev.xkey.state & Mod1Mask && RK == ev.xkey.keycode)
+        system("dmenu_run &");
+      else 
+      if(ev.xkey.state & Mod1Mask && EK == ev.xkey.keycode)
+        return 0;
+    case ButtonPress:
+      if(ev.xbutton.subwindow != None)
+      {
+        XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+        start = ev.xbutton;
+      }
+      break;
+    case MotionNotify:
+      if(start.subwindow != None)
+      {
+        int xdiff = ev.xbutton.x_root - start.x_root;
+        int ydiff = ev.xbutton.y_root - start.y_root;
+        XMoveResizeWindow(dpy, start.subwindow,
+          attr.x + (start.button==1 ? xdiff : 0),
+          attr.y + (start.button==1 ? ydiff : 0),
+          MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
+          MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+      }
+      break;
+    case ButtonRelease:
+      start.subwindow = None;
+      break;
     }
+  }
 }
 
