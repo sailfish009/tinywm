4a60608da8a31ee2287fb21509de74f8eba9c32c
Uriel <uriel@berlinblue.org>
2016-06-16 06:18:35 +0900

    test

--- tinywm.c
+++ tinywm.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#include <algorithm>
 
 int main(void)
 {
@@ -100,8 +100,8 @@ int main(void)
         XMoveResizeWindow(dpy, start.subwindow,
           attr.x + (start.button==1 ? xdiff : 0),
           attr.y + (start.button==1 ? ydiff : 0),
-          MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-          MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+          std::max(1, attr.width + (start.button==3 ? xdiff : 0)),
+          std::max(1, attr.height + (start.button==3 ? ydiff : 0)));
       }
       break;
     case ButtonRelease:
