18624ee5a41f2cdc1a0afeeed50d323e64338d84
Uriel <uriel@berlinblue.org>
2016-10-23 06:43:52 +0900

    fix main()

--- tinywm.c
+++ tinywm.c
@@ -9,20 +9,17 @@ int main(void)
   dpy = XOpenDisplay(0x0);
   SetInput(dpy);
   int fd = XConnectionNumber(dpy);
-  for(;true;)
+  for(;;)
   {
     tv.tv_sec=0; tv.tv_usec=400000;
     FD_ZERO(&set); FD_SET(fd, &set);
     select(fd+1,&set,0,0,&tv);
-    while(XPending(dpy)) 
+    while(XPending(dpy)) XNextEvent(dpy,&ev);
+    switch(ev.type)
     {
-      XNextEvent(dpy,&ev);
-      switch(ev.type)
-      {
-      case KeyPress:ProcessKey(ev);break;
-      case KeyRelease:close = 0;break;
-      default: ProcessMouse(ev);break;
-      }
+    case KeyPress:ProcessKey(ev);break;
+    case KeyRelease:close = 0;break;
+    default: ProcessMouse(ev);break;
     }
   }
   return 0;
